2a88351ccf24af082380f713a28c77990f1dc9f1
Filip Linsbauer <linsbauer.fi@gmail.com>
2025-01-21 22:26:21 +0100

    1

---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <div>
+      <h1 style={{fontSize: 30}} className="text-bold">About</h1>
+    </div>
+    
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 style={{fontSize: 30}} className="text-bold">Home</h1>
+    </div>
+    
+  );
+}
